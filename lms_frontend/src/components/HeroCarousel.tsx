6b05dea8120f304eba07ac9641385a0eb3cf0143
infofitsoftware <infofitsoftware@gmail.com>
2025-11-14 15:56:27 +0530

    Fix: Update loading state to use gradient instead of image

--- lms_frontend/src/components/HeroCarousel.tsx
+++ lms_frontend/src/components/HeroCarousel.tsx
@@ -81,14 +81,15 @@ export default function HeroCarousel() {
       <section className="relative h-[600px] overflow-hidden">
         <div className="relative h-full">
           <div className="absolute inset-0 transition-opacity duration-1000 opacity-100">
-            {/* Background Image */}
+            {/* Background Gradient */}
             <div 
-              className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-              style={{
-                backgroundImage: `url(${heroSlides[0].image})`,
-              }}
+              className={`absolute inset-0 bg-gradient-to-br ${heroSlides[0].gradient}`}
             >
-              <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+              <div className="absolute inset-0 bg-black bg-opacity-30"></div>
+              {/* Decorative pattern overlay */}
+              <div className="absolute inset-0 opacity-10" style={{
+                backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='1'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+              }}></div>
             </div>
             
             {/* Content */}
